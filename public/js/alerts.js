b964f08d277e6a32991ef8bf3142f02dc7ad55f6
Paula Le <pale@ucsd.edu>
2014-02-13 22:40:46 -0800

    trying to figure out jquery stuff for inbox page :(

---
+++ public/js/alerts.js
@@ -0,0 +1,30 @@
+/**
+ * Created by paulale on 2/13/14.
+ */
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+    initializePage();
+})
+
+// Hide social point notification
+$("#accepted").hide();
+
+// Hide alert if ignored button is clicked
+$("#ignore-btn").click(function(){
+    $(this).parent().parent().hide('medium');
+});
+
+$("#accept-btn").click(function(){
+    $(this).parent().parent().hide('medium');
+    $("#accepted").show("slow");
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+    console.log("Javascript connected!");
+
+}
