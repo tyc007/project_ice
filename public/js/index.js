053a6d0d5ad32eebc66fa98978478e9d7eb714d2
Tony <tonychan550@gmail.com>
2014-03-03 15:19:06 -0800

    dynamic find people profile

--- public/js/index.js
+++ public/js/index.js
@@ -9,6 +9,7 @@ var badges = 0;
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
     initializePage();
+
     $('.points').click(function () {
         //var val = Math.floor((Math.random() * 100)) + '%';
 
@@ -29,15 +30,31 @@ $(document).ready(function() {
         $('.progress-bar').width(points + '%').text(points);
 
     })
+
+
+    $('.stranger_profile').click( loadProfile);
+
+    $('.nav-tabs').click( showTab);
+
 })
 
+function loadProfile(e) {
+    e.preventDefault();
+    var page_url=$(this).prop('href');
+    $('.view-profile').load(page_url);
+    $('.tab-content').hide();
+}
 
+function showTab(e) {
+    $('.view-profile').empty();
+    $('.tab-content').show();
+
+}
 
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-    console.log('hi!');
 
 }
